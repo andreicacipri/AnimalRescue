4044c8a659a19275afedf71c487801e5cbbd7eb7
andreicacipri <cipri9412@gmail.com>
2019-01-28 20:03:45 +0200

    Arithmetic Operators

--- src/main/java/org/fasttrackit/Adopter.java
+++ src/main/java/org/fasttrackit/Adopter.java
@@ -25,10 +25,17 @@ public class Adopter {
         this.money = money;
     }
 public void feeding (Animal animal,Food food){
-    System.out.println( getName() + " just gave some " + food.getName() + " to " + animal.getName());
+    System.out.println("Level of hungy is " +animal.hungry);
+        System.out.println( getName() + " just gave some " + food.getName() + " to " + animal.getName());
+      animal.hungry = animal.hungry - 1;
+    System.out.println("New hungry level is: " +animal.hungry );
+
 
     }
     public void recreation(Animal animal, RecreationActivity activity) {
+        System.out.println("Level of happines is " +animal.getSpirit());
         System.out.println(getName() + " go out with " + animal.getName() + " to " + activity.getName());
+        animal.spirit = animal.spirit +1;
+        System.out.println("New level of happines  is: " +animal.spirit );
     }
 }
