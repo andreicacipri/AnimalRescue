92bc61cf38bfe4e1218dea1ec69e1ac3615f147b
andreicacipri <cipri9412@gmail.com>
2019-02-02 20:59:18 +0200

    Final updates

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,7 +1,5 @@
 package org.fasttrackit;
 
-import java.time.LocalDate;
-
 /**
  * Hello world!
  *
@@ -9,64 +7,64 @@ import java.time.LocalDate;
 public class App {
     public static void main(String[] args) {
 
-        Animal firstAnimal = new Animal();
-        firstAnimal.setName("Roky");
-        firstAnimal.setSpirit(5);
-        firstAnimal.setHungry(7);
-        firstAnimal.setHealth(9);
-        firstAnimal.setFavactivity("catch the ball");
-        firstAnimal.setFavfood("Canino Pedigree");
-        Adopter firstAdopter = new Adopter();
-        firstAdopter.setMoney(500);
-        firstAdopter.setName("Andrei");
+//      Animal firstAnimal = new Animal();
+//      firstAnimal.setName("Roky");
+//      firstAnimal.setSpirit(5);
+//      firstAnimal.setHungry(7);
+//      firstAnimal.setHealth(9);
+//      firstAnimal.setFavactivity("catch the ball");
+//      firstAnimal.setFavfood("Canino Pedigree");
+//      Adopter firstAdopter = new Adopter();
+//      firstAdopter.setMoney(500);
+//      firstAdopter.setName("Andrei");
 
-        Food firstFood = new Food();
-        firstFood.setName("Canino Pedigree");
-        firstFood.setPrice(300);
-        firstFood.setQuantity(40);
-        firstFood.setYear(LocalDate.of(2020, 11, 20));
-        firstFood.setInStock(true);
+//      Food firstFood = new Food();
+//      firstFood.setName("Canino Pedigree");
+//      firstFood.setPrice(300);
+//      firstFood.setQuantity(40);
+//      firstFood.setYear(LocalDate.of(2020, 11, 20));
+//      firstFood.setInStock(true);
 
-        System.out.println(firstFood.getYear());
+//      System.out.println(firstFood.getYear());
 
-        RecreationActivity firstRecreation = new RecreationActivity();
-        firstRecreation.setName("catch the ball");
+//      RecreationActivity firstRecreation = new RecreationActivity();
+//      firstRecreation.setName("catch the ball");
 
-        Doctor firstDoctor = new Doctor();
-        firstDoctor.setName("Dr.Marcu");
-        firstDoctor.setSpecialization("dog specialization");
+//      Doctor firstDoctor = new Doctor();
+//      firstDoctor.setName("Dr.Marcu");
+//      firstDoctor.setSpecialization("dog specialization");
 
-        System.out.println(firstDoctor.getName());
-        System.out.println(firstDoctor.getSpecialization());
+//      System.out.println(firstDoctor.getName());
+//      System.out.println(firstDoctor.getSpecialization());
 
-        Animal firstDog = new Dog();
-        firstDog.setName("Rex");
-        firstDog.setHealth(5);
-        firstDog.setHungry(9);
-        firstDog.setSpirit(8);
-        firstDog.setFavactivity("Sleep");
-        firstDog.setFavfood("Canino");
-        ((Dog) firstDog).setInHouse(true);
-        ((Dog) firstDog).setBark(true);
+//      Animal firstDog = new Dog();
+//      firstDog.setName("Rex");
+//      firstDog.setHealth(5);
+//      firstDog.setHungry(9);
+//      firstDog.setSpirit(8);
+//      firstDog.setFavactivity("Sleep");
+//      firstDog.setFavfood("Canino");
+//      ((Dog) firstDog).setInHouse(true);
+//      ((Dog) firstDog).setBark(true);
 
-        Animal firstCat = new Cat();
-        firstCat.setName("Lili");
-        firstCat.setHealth(5);
-        firstCat.setHungry(9);
-        firstCat.setSpirit(7);
-        firstCat.setFavactivity("to purr");
-        firstCat.setFavfood("walkers");
-        ((Cat) firstCat).setInHouse(true);
-        ((Cat) firstCat).setMew(true);
+//      Animal firstCat = new Cat();
+//      firstCat.setName("Lili");
+//      firstCat.setHealth(5);
+//      firstCat.setHungry(9);
+//      firstCat.setSpirit(7);
+//      firstCat.setFavactivity("to purr");
+//      firstCat.setFavfood("walkers");
+//      ((Cat) firstCat).setInHouse(true);
+//      ((Cat) firstCat).setMew(true);
 
         Game newGame = new Game();
-        newGame.setAdopter(firstAdopter);
-        newGame.setDoctor(firstDoctor);
+        //newGame.setAdopter(firstAdopter);
+        //newGame.setDoctor(firstDoctor);
         newGame.start();
-        firstAdopter.feeding(firstAnimal, firstFood);
-        firstAdopter.recreation(firstAnimal,firstRecreation);
-        firstAnimal.spiritlevel(firstAnimal);
-        firstDog.spiritlevel(firstDog);
-        firstCat.spiritlevel(firstCat);
+       // firstAdopter.feeding(firstAnimal, firstFood);
+        //firstAdopter.recreation(firstAnimal,firstRecreation);
+        //firstAnimal.spiritlevel(firstAnimal);
+        //firstDog.spiritlevel(firstDog);
+       // firstCat.spiritlevel(firstCat);
     }
     }
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -2,29 +2,31 @@ package org.fasttrackit;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Game {
    private Adopter adopter;
-   private Dog dog;
+   private Animal animal;
    private Doctor doctor;
     private RecreationActivity[] availableActivities = new RecreationActivity[10];
-   private List<Food> avilableFood = new ArrayList<>();
+   private List<Food> availableFood = new ArrayList<>();
 
     private void initFood() {
         Food firstFood = new Food();
         firstFood.setName("Canino ECO");
-        avilableFood.add(firstFood);
         Food secondFood = new Food();
         secondFood.setName("Whiskas");
-        avilableFood.add(secondFood);
+        availableFood.add(secondFood);
+        availableFood.add(firstFood);
         }
 
-    public void displayAvailableFood() {
+    private void displayAvailableFood() {
         System.out.println(" Available food are :");
-        for (int i = 0; i < avilableFood.size(); i++) {
-            if (avilableFood.get(i) != null)
-                System.out.println(avilableFood.get(i).getName());
-
+        for (int i = 0; i < availableFood.size(); i++) {
+            if (availableFood.get(i) != null) {
+                System.out.println(availableFood.get(i).getName());
+            }
         }
     }
     private void initActivities() {
@@ -39,7 +41,7 @@ public class Game {
     }
 
 
-    public void displayAvailableActivities() {
+    private void displayAvailableActivities() {
         System.out.println("Available activities:");
         for (RecreationActivity recreationActivity : availableActivities) {
             if (recreationActivity != null) {
@@ -47,11 +49,129 @@ public class Game {
             }
         }
     }
-    public void start() {
-        initFood();
-        initActivities();
-        displayAvailableActivities();
+    private String initAnimal() {
+        System.out.println("Available types of Animals are: Cats and Dogs");
+        System.out.println("Please select the type of Animal you want :");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        if (name.equals("Cat")){
+
+            Animal cat = new Cat();
+            int health = ThreadLocalRandom.current().nextInt(1, 10);
+            cat.setHealth(health);
+            int hungry = ThreadLocalRandom.current().nextInt(1, 10);
+            cat.setHungry(hungry);
+            int spirit = ThreadLocalRandom.current().nextInt(1, 10);
+            cat.setSpirit(spirit);
+            cat.setFavactivity("to purr");
+            cat.setFavfood("Wyskas");
+            System.out.println("You choose to adopt a : " + name);
+            animal = cat;
+            return name;
+        }
+        else if (name.equals("Dog")) {
+            Animal dog = new Dog();
+            int health = ThreadLocalRandom.current().nextInt(1, 10);
+            dog.setHealth(health);
+            int hungry = ThreadLocalRandom.current().nextInt(1, 10);
+            dog.setHungry(hungry);
+            int spirit = ThreadLocalRandom.current().nextInt(1, 10);
+            dog.setSpirit(spirit);
+            dog.setFavactivity("caught the ball");
+            dog.setFavfood("Pedigree ECO");
+            animal = dog;
+            System.out.println("You choose to adopt a : " + name);
+            return name;
+        }
+            System.out.println("Please try again ");
+            return initAnimal();
+
+
+    }
+    private String initAdopter() {
+
+        System.out.println("Please insert your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        Adopter adopter = new Adopter();
+        adopter.setName(name);
+        System.out.println("Please insert your money: ");
+        Scanner scanner1 = new Scanner(System.in);
+        double money = scanner1.nextDouble();
+        adopter.setMoney(money);
+
+        System.out.println("I am  " + adopter.getName()+" and I have "+ adopter.getMoney()+" Dollars");
+    return name;
+    }
+
+    private String nameAnimal() {
+        System.out.println("Please insert animal name: ");
+        Scanner scanner = new Scanner(System.in);
+        String animalName = scanner.nextLine();
+        animal.setName(animalName);
+        System.out.println("Your animal name: "+animal.getName()+", health level: "+animal.getHealth()+ ", hungry level: "
+                +animal.getHungry()+ ", spirit level: "+animal.getSpirit()+", Favorite activity: "+animal.getFavactivity()+ ", and Favorite food: "
+                +animal.getFavfood());
+        return animalName;
+    }
+    private String requireFeeding() {
         displayAvailableFood();
+        System.out.println("Please select a food:");
+        Scanner scanner = new Scanner(System.in);
+        String selectFood = scanner.nextLine();
+        if (selectFood.equals(animal.getFavfood())) {
+            animal.hungry = animal.hungry + 2;
+            animal.health = animal.health-1;
+        }
+        animal.hungry = animal.hungry + 1;
+        animal.health = animal.health-1;
+        if (animal.hungry >= 10) {
+            animal.setHungry(10);
+            animal.spirit = animal.spirit - 2;
+            if (animal.spirit >= 10) {
+                animal.setSpirit(10);
+                animal.spirit = animal.spirit - 2;
+
+            }
+        }
+        System.out.println("Your animal name: "+animal.getName()+", health level: "+animal.getHealth()+ ", hungry level: "
+                +animal.getHungry()+ ", spirit level: "+animal.getSpirit()+", Favorite activity: "+animal.getFavactivity()+ ", and Favorite food: "
+                +animal.getFavfood());
+    return selectFood;
+    }
+    private String requireActivity() {
+        displayAvailableActivities();
+        System.out.println("Please select an activity:");
+        Scanner scanner = new Scanner(System.in);
+        String selectActivity = scanner.nextLine();
+        if (selectActivity.equals(animal.getFavactivity())) {
+            animal.health = animal.health+ 2;
+            animal.hungry = animal.hungry -3;
+        }
+        animal.health = animal.health + 1;
+        animal.hungry = animal.hungry -2;
+        if (animal.health >= 10) {
+            animal.setHealth(10);
+            animal.spirit = animal.spirit - 2;
+            if (animal.spirit >= 10) {
+                animal.setSpirit(10);
+                animal.spirit = animal.spirit - 2;
+            }
+        }
+        System.out.println("Your animal name: "+animal.getName()+", health level: "+animal.getHealth()+ ", hungry level: "
+                +animal.getHungry()+ ", spirit level: "+animal.getSpirit()+", Favorite activity: "+animal.getFavactivity()+ ", and Favorite food: "
+                +animal.getFavfood());
+        return selectActivity;
+    }
+    public void start() {
+        initAnimal();
+        initAdopter();
+        nameAnimal();
+
+        requireFeeding();
+        requireActivity();
+
     }
         public Adopter getAdopter() {
         return adopter;
@@ -61,12 +181,12 @@ public class Game {
         this.adopter = adopter;
     }
 
-    public Dog getDog() {
-        return dog;
+    public Animal getAnimal() {
+        return animal;
     }
 
-    public void setDog(Dog dog) {
-        this.dog = dog;
+    public void setDog(Animal animal) {
+        this.animal = animal;
     }
 
     public Doctor getDoctor() {
